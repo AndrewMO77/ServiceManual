e1e60f4317e2c5f0ce74742d8b88005809229c15
Andrew Marriner <amo-tut@outlook.com>
2021-01-18 00:04:32 +0200

    Minor fix to typo in MaintenanceController URL

--- src/main/java/com/etteplanmore/servicemanual/controller/MaintenanceController.java
+++ src/main/java/com/etteplanmore/servicemanual/controller/MaintenanceController.java
@@ -27,7 +27,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@RequestMapping(ControllerHelper.apiBaseUrl + "/maitenance")
+@RequestMapping(ControllerHelper.apiBaseUrl + "/maintenance")
 public class MaintenanceController {
 
 	@Autowired
